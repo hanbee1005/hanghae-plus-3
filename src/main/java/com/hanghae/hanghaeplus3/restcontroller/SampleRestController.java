283f4370b2264c166a9640a001ba2f93b1d8a338
hanbee <sonhanbi1002@gmail.com>
2023-12-03 10:54:26 +0900

    feat: add sample api

---
+++ src/main/java/com/hanghae/hanghaeplus3/restcontroller/SampleRestController.java
@@ -0,0 +1,13 @@
+package com.hanghae.hanghaeplus3.restcontroller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleRestController {
+    @GetMapping("/")
+    public ResponseEntity<?> helloWorld() {
+        return ResponseEntity.ok("Hello World!!");
+    }
+}
