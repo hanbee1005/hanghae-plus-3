18b19e75ae0c143ab854602db0b91dd8bf9bed88
hanbee <sonhanbi1002@gmail.com>
2023-12-03 10:33:06 +0900

    init

---
+++ src/main/java/com/hanghae/hanghaeplus3/HanghaePlus3Application.java
@@ -0,0 +1,13 @@
+package com.hanghae.hanghaeplus3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaePlus3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanghaePlus3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanghae/hanghaeplus3/HanghaePlus3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae.hanghaeplus3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanghaePlus3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
