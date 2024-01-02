0ba4a3e3358916811413272985cd72c7ebdc85ad
hanbee <sonhanbi1002@gmail.com>
2024-01-03 00:35:34 +0900

    feat: test 용 yml 설정

--- src/test/java/com/hanghae/hanghaeplus3/HanghaePlus3ApplicationTests.java
+++ src/test/java/com/hanghae/hanghaeplus3/HanghaePlus3ApplicationTests.java
@@ -1,10 +1,8 @@
 package com.hanghae.hanghaeplus3;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Disabled
 @SpringBootTest
 class HanghaePlus3ApplicationTests {
 
