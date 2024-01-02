e08136b94df544e1eca86d0c86e4ad8232ad6a30
hanbee <sonhanbi1002@gmail.com>
2024-01-03 00:22:49 +0900

    feat: db 사용하는 테스트 disabled (임시)

--- src/test/java/com/hanghae/hanghaeplus3/HanghaePlus3ApplicationTests.java
+++ src/test/java/com/hanghae/hanghaeplus3/HanghaePlus3ApplicationTests.java
@@ -1,8 +1,10 @@
 package com.hanghae.hanghaeplus3;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class HanghaePlus3ApplicationTests {
 
