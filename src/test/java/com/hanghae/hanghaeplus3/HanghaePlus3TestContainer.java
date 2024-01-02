2bcc10db4977daae35de46dcc176c24901c60d6f
hanbee <sonhanbi1002@gmail.com>
2024-01-03 00:45:53 +0900

    feat: testcontainer redis host,port 설정

--- src/test/java/com/hanghae/hanghaeplus3/HanghaePlus3TestContainer.java
+++ src/test/java/com/hanghae/hanghaeplus3/HanghaePlus3TestContainer.java
@@ -1,6 +1,8 @@
 package com.hanghae.hanghaeplus3;
 
 import com.redis.testcontainers.RedisContainer;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -9,9 +11,15 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 public class HanghaePlus3TestContainer {
     private static final String MYSQL_VERSION = "mysql:8";
 
-//    @Container
-//    public static final MySQLContainer MYSQL_CONTAINER = new MySQLContainer(MYSQL_VERSION);
-//
-//    @Container
-//    public static final RedisContainer REDIS_CONTAINER = new RedisContainer(RedisContainer.DEFAULT_IMAGE_NAME.withTag(RedisContainer.DEFAULT_TAG));
+    @Container
+    public static final MySQLContainer MYSQL_CONTAINER = new MySQLContainer(MYSQL_VERSION);
+
+    @Container
+    public static final RedisContainer REDIS_CONTAINER = new RedisContainer(RedisContainer.DEFAULT_IMAGE_NAME.withTag(RedisContainer.DEFAULT_TAG));
+
+    @DynamicPropertySource
+    public static void overrideProps(DynamicPropertyRegistry registry){
+        registry.add("spring.data.redis.host", REDIS_CONTAINER::getHost);
+        registry.add("spring.data.redis.port", () -> ""+REDIS_CONTAINER.getMappedPort(6379));
+    }
 }
