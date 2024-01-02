4b302dac6860efbe4b915728e69b28c59f174a7e
hanbee <sonhanbi1002@gmail.com>
2024-01-03 00:54:49 +0900

    feat: testcontainer 상속

--- src/test/java/com/hanghae/hanghaeplus3/HanghaePlus3ApplicationTests.java
+++ src/test/java/com/hanghae/hanghaeplus3/HanghaePlus3ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HanghaePlus3ApplicationTests {
+class HanghaePlus3ApplicationTests extends HanghaePlus3TestContainer {
 
     @Test
     void contextLoads() {
